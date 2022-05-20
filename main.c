60222e5f9651b65eea8b14227acb7118ac846740
brahimjarallah <brahim.jarallah@gmail.com>
2022-05-20 12:09:31 +0200

    corrected

--- main.c
+++ main.c
@@ -1,33 +1,24 @@
 #include "stm32f4xx.h" // Device header
-// PD12 // LD4
+// PD15 // LD6
 
-//void delay(void);
-void delay(int delay);
+void delay(void);
+// void delay(int delay);
 
 int main(void){
 	
-	RCC->AHB1ENR |= 1000;  // 1bit/input  /16bits
-	GPIOD->MODER |= 0x1000000; // 2bits/input /32bits
+	RCC->AHB1ENR |= 8;  // 1bit/input  /16bits
+	GPIOD->MODER |= 0x40000000; // 2bits/input /32bits
 	
 	while(1){
-		GPIOD->BSRR = 1000u;  // 1bit/input  /16bits
-		delay(1000);
-		GPIOD->BSRR = 10000000u;
-		delay(1000);
+		GPIOD->BSRR = 0x8000u;  // 1bit/input  /16bits
+		delay();
+		GPIOD->BSRR = 0x80000000u;
+		delay();
 	}	
 }	
 
-
-void delay(int delay)
-{ int i ;
-for(; delay>0; delay--)
-{
-for(i=0; i<4000; i++);
+void delay(void){
+	volatile int i;
+for(i=0;i<500000;i++);
 }
-}
-
-//void delay(void){
-//	volatile int i;
-//for(i=0;i<500000;i++);
-//}
 		
